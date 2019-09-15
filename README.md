4a034d00cfe26d4dbdd2b50484e6ad712e724435
darren2025 <darren@321dz.com>
2019-09-15 23:35:33 +0800

    添加说明文件

---
+++ README.md
@@ -0,0 +1,42 @@
+# 一、使用说明:
+
+## 1.基本使用:
+
+```linux
+#下载文件
+wget https://github.com/darren2025/mysql5.1-5.6/blob/master/install_mysql.sh
+#给予权限
+chmod +x install_mysql.sh
+#执行:
+sh install_mysql.sh
+```
+
+执行后除了第二个设置密码的,请填写密码,其他都默认y
+
+## 2.开启自启
+
+如果需要加入开机启动项:请接着往下看
+
+### centos6+版本
+
+执行以下
+
+```linux
+chkconfig --add mysqld
+chkconfig --level 35 mysqld on
+```
+
+### centos7+
+
+```linux
+systemctl enable nginx #设置开机启动
+```
+
+## 3.添加到防火墙允许外网访问
+
+```
+iptables -I INPUT -p tcp --dport 3306 -j ACCEPT
+service iptables save 【固定格式：保存配置】
+service iptables restart 【固定格式：重启防火墙服务】
+```
+
--- install_mysql.sh
+++ install_mysql.sh
@@ -4,10 +4,10 @@ yum -y remove mysql mysql-server mysql-libs
 #删除本地的mysql残留
 rm -fr /var/lib/mysql/*
 # 下载远程的包
-wget "https://repo.mysql.com/yum/mysql-5.6-community/el/6/i386/mysql-community-server-5.6.45-2.el6.i686.rpm"
-wget "https://repo.mysql.com/yum/mysql-5.6-community/el/6/i386/mysql-community-client-5.6.45-2.el6.i686.rpm"
-wget "https://repo.mysql.com/yum/mysql-5.6-community/el/6/i386/mysql-community-common-5.6.45-2.el6.i686.rpm"
-wget "https://repo.mysql.com/yum/mysql-5.6-community/el/6/i386/mysql-community-libs-5.6.45-2.el6.i686.rpm"
+wget "https://raw.githubusercontent.com/darren2025/mysql5.1-5.6/master/mysql-community-client-5.6.45-2.el6.i686.rpm"
+wget "https://raw.githubusercontent.com/darren2025/mysql5.1-5.6/master/mysql-community-common-5.6.45-2.el6.i686.rpm"
+wget "https://raw.githubusercontent.com/darren2025/mysql5.1-5.6/master/mysql-community-libs-5.6.45-2.el6.i686.rpm"
+wget "https://raw.githubusercontent.com/darren2025/mysql5.1-5.6/master/mysql-community-server-5.6.45-2.el6.i686.rpm"
 # 安装
 rpm -Uvh mysql-community-common-5.6.45-2.el6.i686.rpm
 # 安装
